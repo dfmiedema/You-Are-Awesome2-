79a93c7cb15484155f8ab0ecf29610d2ab670c17
Davis Miedema <francisco117.j@gmail.com>
2022-02-28 17:58:22 -0800

    Complete 2.11

--- You Are Awesome2!/ViewController.swift
+++ You Are Awesome2!/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImage = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +24,22 @@ class ViewController: UIViewController {
         let messages = ["You Are Awesome!",
                         "You Are Great!",
                         "You Are Fantastic!",
+                        "When the Genius Bar Needs Help, They Call You!",
+                        "You've Got The Design Skills of Jony Ive",
                         "Fabulous? That's You!"]
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImage))")
+        //        messageLabel.text = messages[messageNumber]
+        //        messageNumber += 1
+        //        if messageNumber == messages.count {
+        //            messageNumber = 0
+        //        }
         
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
-        
-        let imageName = "image" + String(imageNumber)
-        imageView.image = UIImage(named: imageName )
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
+//        let imageName = "image" + String(imageNumber)
+//        imageView.image = UIImage(named: imageName )
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 10 {
+//            imageNumber = 0
         }
         
         //        let greatMessage = "You Are Great!"
@@ -52,4 +56,4 @@ class ViewController: UIViewController {
         //            imageView.image = UIImage(named: "image0")
         //        }
     }
-}
+
