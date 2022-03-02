98801d4b87c351d7a5eeb2d6321f75adc0dd06cb
Davis Miedema <francisco117.j@gmail.com>
2022-03-02 12:05:30 -0800

    Complete 2.15

--- You Are Awesome2!/ViewController.swift
+++ You Are Awesome2!/ViewController.swift
@@ -5,7 +5,10 @@
 //  Created by Francisco Jimenez III on 2/21/22.
 //
 
+
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
@@ -14,6 +17,7 @@ class ViewController: UIViewController {
     var imageNumber = -1
     var messageNumber = -1
     let totalNumberOfImage = 9
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +45,17 @@ class ViewController: UIViewController {
         } while imageNumber == newImageNumber
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "image\(imageNumber)")
+        
+        if let sound = NSDataAsset(name: "sound0") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("😡ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object.")
+            }
+        } else {
+            print("😡ERROR: Could not read data from file sound0")
+        }
     }
 
 }
